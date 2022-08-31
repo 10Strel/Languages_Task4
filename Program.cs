b85d21482a1195a39d177abdea4ae8cbf917c3b0
10Strel <manowartenarrows@gmail.com>
2022-08-31 20:08:34 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Напишите программу, которая принимает на вход три числа\r\nи выдаёт максимальное из этих чисел.\r\n");
