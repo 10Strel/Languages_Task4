c869143d95c59862c3ff3f5c3d9d4a526acf6e89
10Strel <manowartenarrows@gmail.com>
2022-08-31 20:21:19 +0300

    Final commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Напишите программу, которая принимает на вход три числа\r\nи выдаёт максимальное из этих чисел.\r\n");
+﻿Console.WriteLine("Напишите программу, которая принимает на вход три числа\r\nи выдаёт максимальное из этих чисел.\r\n");
+
+Console.WriteLine("Введите три числа (разделитель - запятая):");
+
+string[] numbers = Console.ReadLine().Split(new char[]{','});
+
+if (numbers.Length != 3)
+{
+    Console.WriteLine("Количество введенных чисел не равно 3.\r\nВыполнение программы будет остановлено.");
+    return;
+}
+
+Console.WriteLine($"Максиальное введенное число: {(numbers.Max())}");
